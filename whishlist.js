11ca748fa672890c1d9a19e7e2116c73ce16ed84
Xandary <61241724+Xandary@users.noreply.github.com>
2021-06-20 19:39:50 +0200

    add souc, update yumpa feller

--- whishlist.js
+++ whishlist.js
@@ -13,9 +13,9 @@ const app = new Vue({
         activeBoss: 'Karazhan'
     },
     created: function () {
-        this.db = db;
-        // this.prepareDb();
-        // this.fetchWhishlist().then(this.sort).then(() =>console.log(JSON.stringify(this.db)));
+        // this.db = db;
+        this.prepareDb();
+        this.fetchWhishlist().then(this.sort).then(() =>console.log(JSON.stringify(this.db)));
     },
     methods: {
         prepareDb() {
@@ -72,17 +72,18 @@ const app = new Vue({
             await this.fillDb('Zelion', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRE45pTn40bF-WZdxE9_wMF48kkTiN-3bvH7ffkjBg1QbYK0MW6Mwt9WdzwaI2t4eMpQucSOSrmz1kY/pub?gid=0&single=true&output=csv');
             await this.fillDb('Taurnil', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vStGEJ9Be5HDTlrxLI5PFmNUnTeFfrPx-PXn7TY7VEsbUSCWkF4poysA29Lpyl6cmAAa4DVRcwF6kht/pub?gid=0&single=true&output=csv');
             await this.fillDb('Dojila', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTlBZMcxJLYuxPhgl_eUlwXCo0OAYuzBt7QMlYm2huAeiMQG2ECqSZbWGuNClDoPQlM3aldbQvIU-aL/pub?gid=0&single=true&output=csv');
-            await this.fillDb('Fellerson', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRub7bdZaJjs-sfCsFfSv1bgLheKuLu0-W_7VrEKT9ZKcigZickSyuXyVylfX1Z3aR4hs7zW4qt1EUx/pub?gid=0&single=true&output=csv');
+            await this.fillDb('Fellerson', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT7dS0-dsvdszZubdh7tt_aM7hsWjIN9Czx6kOoxwzTkMT-N2bR_6W-PEUpSlVkLNFOd6uO-d650jHG/pub?gid=0&single=true&output=csv');
             await this.fillDb('Mareeka', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ77i0sIPhnFh5rzFPW2yY9EUSB1ybGKm2OE8-b0emFX88C8glL_3bbZFzJQ1hmnH58xQs8me2RZVbz/pub?gid=0&single=true&output=csv');
             await this.fillDb('Kinua', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhr6RihR2PtQSAucCrENVHhZUqggDrUs3dNtHlm3fM_9VQDPwytP0uFfSsuWvL9qWWw0IUzPA-mPt3/pub?gid=0&single=true&output=csv');
             await this.fillDb('Jahaag', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTy_P8RzOi-0_t-1DpusccQt26tXGIuPUSTmK2QiQLTwD4J5xVmxGYv-yXJYbTg9pXJOB6mUokYii5p/pub?gid=0&single=true&output=csv');
             await this.fillDb('Geb', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRIn7K536DNKZppadip49PmODs7Yo-1bY8mGVz9-KIm2EyDZC5UzHPwazx9enE7Vp-dyXFRFOjpGSz5/pub?gid=0&single=true&output=csv');
             await this.fillDb('barbieturic', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRXpdcwlgLNTxSvtTXJ9XWnvC0JQcUwaskK9CpueZTxeSKwGI-KkYzDHv5gwAi7WDCJqBk1mfw_p_a/pub?gid=0&single=true&output=csv');
-            await this.fillDb('Yumpala', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTa8IJARC-48yH1gcK9lLL0CIF2J8ws_yftnB-yKaY86uSyCCfNABvgQEGzcgtKxvwbhpFh1GrWIN0u/pub?gid=0&single=true&output=csv');
+            await this.fillDb('Yumpala', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTGyOYesSO8zghrPr_KQVW5oQLMgVqyzJxA0PU-uax3Yen6IzP686MZRQQFpeO7HJ4IqtfSwdU2ZfeK/pub?gid=0&single=true&output=csv');
             await this.fillDb('Sailar', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRfAA4WH9s8k8BMml4q9-OFcU-yBGHeUUPdnPdrE2wsvQyMdV1lfyAMMHuCQ_TzvibHnDarwRbRxHD4/pub?gid=0&single=true&output=csv');
             await this.fillDb('Taurache', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTQ4bs3R4qOOzawBkpm3UNGXlIkwMbuYmrYhBDk-tGGscP6GAiOyEVR8_NBUuRDNLDZZZ3x00KTyVdh/pub?gid=0&single=true&output=csv');
             await this.fillDb('Tochtoch', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4EYvimudtT6L4P9U0pQsru-j3xYwHC5dRjNctnSvhj1YGNB7V2A75U0lMWlsV8j2ssiIYSGK0Udpg/pub?gid=0&single=true&output=csv');
             await this.fillDb('Reps', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9oahxsUAzQmJ6K-hZRhPY7O895OzT6a0eKEa5wYXfryYJ7j_CyamdCEI8c0JhyodNCCLR5UzBlj_E/pub?gid=0&single=true&output=csv');
+            await this.fillDb('souc', 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTrOCuSvtLnoBk8Ck-NrIyVRCFGe1CHNbXpW3imJnJNzYryuIEB_Ydyp0OG12gUA8hBYNQ7zo54d7oT/pub?gid=0&single=true&output=csv');
         },
         
         
